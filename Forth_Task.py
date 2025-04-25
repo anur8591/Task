8c1918a452c29e153b245c2c6d619ba9647cffed
Anurag Pal <palanurag595@gmail.com>
2025-04-25 23:02:07 +0530

    task four completed

--- Forth_Task.py
+++ Forth_Task.py
@@ -1,1 +1,15 @@
 print("🌡️ Temperature Converter 🌡️")
+print("1. Celsius to Fahrenheit")
+print("2. Fahrenheit to Celsius")
+choice = input("enter 1 or 2: ")
+
+temp = float(input("enter the temperature: "))
+
+if choice == 1:
+    fahre = (temp * 9/5) + 32
+    print(f"{temp}°C = {fahre:.2f}°F")
+elif choice == 2:
+    cels = (temp - 32) * 5/9
+    print(f"{temp}°C = {cels:.2f}°F")
+else:
+    print("Invalid choice. please enter 1 or 2")
